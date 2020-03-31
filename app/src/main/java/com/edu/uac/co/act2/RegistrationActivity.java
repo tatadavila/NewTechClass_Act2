bd5b656578ce39de097a549d7cb8745ea9d728c6
your usernametatadavila <tata.davila.a@gmail.com>
2020-03-31 02:21:49 -0500

    Created registration activity and layout

---
+++ app/src/main/java/com/edu/uac/co/act2/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.edu.uac.co.act2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
